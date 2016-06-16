daf9427920cf3ddc3fac910e63e406377a7dbac0
Janpo Pan <janpo.pan@gmail.com>
2016-06-16 13:10:15 +0800

    let the old version working

--- scripts/clone_pingpp_sdk.js
+++ scripts/clone_pingpp_sdk.js
@@ -10,7 +10,7 @@ module.exports = function(ctx) {
     , style = r('ansi-styles')
     , platform = ctx.opts.plugin.platform
     , sdk_path = path.join(ctx.opts.plugin.dir, 'sdk', platform)
-    , sdk_url = platform ==='ios' ? 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git#61cdfaee2018b89929683c3411541d509a4cad4f' : 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git';
+    , sdk_url = platform ==='ios' ? 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git#61cdfaee2018b89929683c3411541d509a4cad4f' : 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git#b0c4d23a138622d559c777e48048eb759d1c2872';
 
   if (shell.test('-e', sdk_path)) { d.resolve(); }
   else {
